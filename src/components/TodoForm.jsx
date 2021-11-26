cde561e2a67d361857b5463e7057a9ad59b62ed9
ryan <ryanbroguiere123@gmail.com>
2021-11-26 14:11:35 -0800

    404 error with netlify

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-const url = "http://localhost:4000"
+//const url = "http://localhost:4000"
+const url = "https://productivity-list.netlify.app";
 
 const TodoForm = () => {
     const [inputText, setInputText] = useState('')
