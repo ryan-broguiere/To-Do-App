89818d2d730fc5f8d129288bdf4c7ea6c51071e8
ryan <ryanbroguiere123@gmail.com>
2021-11-26 20:36:26 -0800

    cant deploy ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
     return (
-<footer className="flex text-gray-100 bg-blue-400 dark:text-white dark:bg-black body-font sticky sticky-footer bottom-0 w-full ">
+<footer className="flex text-gray-100 bg-blue-400 dark:text-white dark:bg-black body-font sticky sticky-footer w-full ">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col ">    
     <img className="border-rounded dark:border-blue-400 dark:bg-blue-400 border-2 rounded-full transition duration-200 transform hover:scale-110" src="https://img.icons8.com/cute-clipart/64/000000/check.png" height="48" width="48" alt=''/>
       <span className="ml-3 text-xl">ToDo App</span>    
