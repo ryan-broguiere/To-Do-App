77c4bc6c499b3fd49cb1b25fe9a841f74d48cc3c
Sam <huy.le09@yahoo.com>
2021-11-27 19:32:39 -0800

    Fixed minor things

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,27 @@
 const cors = require("cors");
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config({ path: "./db.env" })
+require("dotenv").config({ path: "./db.env" });
 //const serverless = require('serverless-http');
 const port = process.env.PORT || 4000;
-app.use(express.json())
-app.use(cors())
-app.use(require("./routes"))
+app.use(express.json());
+app.use(cors());
+app.use(require("./routes"));
 
-app.get('/', (req,res) => {
-    response.send('this is a response')    
-})
+app.get("/", (req, res) => {
+    res.send("this is a response");
+});
 
-
-mongoose.connect(process.env.DB_URL,
-  {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+    useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-  console.log("Connected successfully to MongoDB");
+    console.log("Connected successfully to MongoDB");
 });
 
-
-app.listen(port, () => console.log(`Server running on Localhost:${port}`))
+app.listen(port, () => console.log(`Server running on Localhost:${port}`));
